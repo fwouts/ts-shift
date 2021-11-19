04380d1513109a10174c236a479a843482755647
François Wouts <f@zenc.io>
2021-11-20 09:45:51 +1100

    Set up CLI

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
+import cac from "cac";
+import chalk from "chalk";
 import { writeFileSync } from "fs";
 import path from "path";
 import { generate } from "./lib/generator";
 import { parse } from "./lib/parser";
 
-const types = parse([path.join(__dirname, "..", "examples", "user.ts")]);
-const generated = generate(types);
-writeFileSync(
-  path.join(__dirname, "..", "generated", "user.ts"),
-  generated,
-  "utf8"
-);
+const cli = cac("ts-shift");
+
+cli
+  .command("<entry>") // default command
+  .alias("generate")
+  .option("-o, --output <path>", "Output path of the generated module")
+  .action(
+    async (
+      entry: string,
+      options: {
+        o?: string;
+      }
+    ) => {
+      const types = parse([path.resolve(entry)]);
+      const generated = generate(types);
+      if (options.o) {
+        writeFileSync(path.resolve(options.o), generated, "utf8");
+        console.log(`${chalk.green("Generated:")} ${options.o}`);
+      } else {
+        console.log(generated);
+      }
+    }
+  );
+
+cli.help();
+cli.version(require("../package.json").version);
+
+cli.parse();
